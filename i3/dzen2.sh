3c89f8fa4e3bf7319a96e02f9be4b1d62013eaf1
Stefano Zaghi <stefano.zaghi@gmail.com>
2015-06-10 09:17:32 +0200

    try to sanitize

--- i3/dzen2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-HEIGHT=12
-WIDTH=0
-RESOLUTION=$(xrandr | grep " connected" | cut -d" " -f 3)
-RESOLUTIONW=$(echo $RESOLUTION | cut -d"x" -f1)
-RESOLUTIONH=$(echo $RESOLUTION | cut -d"x" -f2 | cut -d'+' -f 1)
-X=$(($RESOLUTIONW-$WIDTH))
-Y=$(($RESOLUTIONH-$HEIGHT-1))
